7f26bb543fda015bde7f29a066a49f9dbcf96924
andressamorschel <andressamorschel@gmail.com>
2022-05-25 15:18:58 -0300

    algortimo finalizado

--- src/br/com/atividade1/Service.java
+++ src/br/com/atividade1/Service.java
@@ -1,29 +1,33 @@
 package br.com.atividade1;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Service {
+
     public void test() {
         System.out.println("Digite a quantidade de apartamentos:");
 
         Scanner scan = new Scanner(System.in);
-        int maiorQuantidade = 0;
-        int indiceMaiorQuantidade = 0;
         int quantidadeApartamento = scan.nextInt();
 
-        int[] consumoPorApartamento = new int[quantidadeApartamento];
+        double maiorQuantidade = 0;
+        int indiceMaiorGastoPorAp = 0;
+        double[] consumoPorApartamento = new double[quantidadeApartamento];
 
         for (int i = 0; i < quantidadeApartamento; i++) {
             System.out.println(String.format("Digite o consumo em litros do ap %d", i));
-            consumoPorApartamento[i] = scan.nextInt();
-            if(consumoPorApartamento[i] > maiorQuantidade){
+            consumoPorApartamento[i] = scan.nextDouble();
+            if (consumoPorApartamento[i] > maiorQuantidade) {
                 maiorQuantidade = consumoPorApartamento[i];
-                indiceMaiorQuantidade = consumoPorApartamento[i];
+                indiceMaiorGastoPorAp = i;
             }
         }
-        System.out.println(Arrays.toString(consumoPorApartamento));
-        System.out.println(indiceMaiorQuantidade);
+        var consumoEmMetroCubico = calcularMetroCubico(maiorQuantidade);
+        System.out.println(String.format("O maior consumo foi de %2f do apartamento %d", consumoEmMetroCubico,
+                indiceMaiorGastoPorAp));
+    }
 
+    public double calcularMetroCubico(double quantidadeEmLitros) {
+        return quantidadeEmLitros / 1000;
     }
 }
